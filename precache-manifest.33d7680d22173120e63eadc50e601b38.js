2302fe2a08347e7c3927c2b952b5e38f1c73e1a7
McElectric29 <jt0729@stanford.edu>
2020-09-01 23:55:24 -0700

    Updates

---
+++ precache-manifest.33d7680d22173120e63eadc50e601b38.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda20211f49f36d704d56d7cab89c3f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a944a02b00164bf79d9c",
+    "url": "/static/css/main.9a808c53.chunk.css"
+  },
+  {
+    "revision": "aeaac504f1af4be54f11",
+    "url": "/static/js/2.dcfbeeb8.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.dcfbeeb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a944a02b00164bf79d9c",
+    "url": "/static/js/main.6bea06bc.chunk.js"
+  },
+  {
+    "revision": "fbe05e49fb1621333d72",
+    "url": "/static/js/runtime-main.b8766504.js"
+  },
+  {
+    "revision": "68f6424db6ed3b54c3b3b586b9618ee9",
+    "url": "/static/media/aispy1.68f6424d.png"
+  },
+  {
+    "revision": "e62da0189adae206a81a373c15993d37",
+    "url": "/static/media/aispy2.e62da018.png"
+  },
+  {
+    "revision": "82808704b8a2a93fea6e7dd70a516237",
+    "url": "/static/media/aispy3.82808704.png"
+  },
+  {
+    "revision": "f2d60799520d1ae79d3a94d546095200",
+    "url": "/static/media/aispy4.f2d60799.png"
+  },
+  {
+    "revision": "00b6c1321e462b878f68114a9cd2470c",
+    "url": "/static/media/aispysc.00b6c132.png"
+  },
+  {
+    "revision": "310a022e920e49c800e480bca0f3508f",
+    "url": "/static/media/animebg.310a022e.jpg"
+  },
+  {
+    "revision": "4e2df6b096617e292d7808db03af6cdc",
+    "url": "/static/media/animebg2.4e2df6b0.png"
+  },
+  {
+    "revision": "85054a2a23cf4870a690da2c3c25f2d0",
+    "url": "/static/media/animebg3.85054a2a.jpg"
+  },
+  {
+    "revision": "cd30d37bf6bc62dc6f6d5a755ebdcd46",
+    "url": "/static/media/greenhrm.cd30d37b.png"
+  },
+  {
+    "revision": "834804ccc9f7eb68435f46be44dd09d2",
+    "url": "/static/media/hcifinal.834804cc.png"
+  },
+  {
+    "revision": "3ab832ae3e7dcf0c1c3e749ddc568739",
+    "url": "/static/media/itzone.3ab832ae.jpg"
+  },
+  {
+    "revision": "7308a85712f95581e6bb830f940eb7f7",
+    "url": "/static/media/profile.7308a857.jpg"
+  },
+  {
+    "revision": "b1acb5770dbd62f9a6fdb3cad2f98855",
+    "url": "/static/media/smartprimer.b1acb577.jpg"
+  },
+  {
+    "revision": "9a3f4b867b6047e71e39c79ef9d0b90a",
+    "url": "/static/media/snackchat1.9a3f4b86.png"
+  },
+  {
+    "revision": "9742c003f29161707753627fc019552f",
+    "url": "/static/media/snackchat2.9742c003.png"
+  },
+  {
+    "revision": "de21a8922fd19bbbe133b8b7d5441a33",
+    "url": "/static/media/snackchat3.de21a892.png"
+  },
+  {
+    "revision": "d6c1bc6731cca6702fb8a1dc2084e259",
+    "url": "/static/media/snackchatsc.d6c1bc67.png"
+  },
+  {
+    "revision": "f324e820bd3cec0c3573fa0f662a993d",
+    "url": "/static/media/website.f324e820.png"
+  },
+  {
+    "revision": "cc3f4408f118d4ebde06c5136ced1e71",
+    "url": "/static/media/wesearch1.cc3f4408.png"
+  },
+  {
+    "revision": "53101de683d113653f945379f7952d84",
+    "url": "/static/media/wesearch2.53101de6.png"
+  },
+  {
+    "revision": "2d32e3b6c2af07405a539aac67d19dba",
+    "url": "/static/media/wesearch3.2d32e3b6.png"
+  },
+  {
+    "revision": "443dfe161dd16619210b795d52315213",
+    "url": "/static/media/wesearch4.443dfe16.png"
+  },
+  {
+    "revision": "97b978e041710f3502e1f9995196eca5",
+    "url": "/static/media/wesearchsc.97b978e0.png"
+  }
+]);
